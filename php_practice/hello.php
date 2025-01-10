44e33ae7e699ac26033f3e2298d622f04b86e720
Luis Torres <142362616+ltorre701@users.noreply.github.com>
2025-01-10 00:28:45 +0000

    end of Jan 9

---
+++ php_practice/hello.php
@@ -0,0 +1,9 @@
+<?php
+
+$myName = "Luis";
+$myFriend = "Andrew";
+echo "Hello, World! \n";
+echo "My name is $myName \n";
+echo "Hello $myFriend \n";
+echo "Hello $myFriend \n";
+echo "Hello $myFriend"." How are you? \n";
